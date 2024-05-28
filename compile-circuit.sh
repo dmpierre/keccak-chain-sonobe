a5031d09b65740f342c7b81e51b3ef3d93dd60e2
dmpierre <pdaixmoreux@gmail.com>
2024-05-28 10:37:38 +0200

    feat: add `lib.rs` with running example

--- compile-circuit.sh
+++ compile-circuit.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+COMPILE_KECCAK_CHAIN=${1}
+if [ ! -d "circuit" ]; then
+    mkdir circuit
+    cd circuit
+    npm install
+    cd ..
+fi
 
-# rm previous files
-rm -r ./circuit/keccak-chain_js
-rm circuit/keccak-chain.r1cs
-rm circuit/keccak-chain.sym
+if [ "$COMPILE_KECCAK_CHAIN" = "true" ]; then
+    circom ./circuit/keccak-chain.circom --r1cs --json --sym --wasm --prime bn128 --output ./circuit/
+fi
 
-cd circuit
-npm install
-cd ..
-
-circom ./circuit/keccak-chain.circom --O0 --r1cs --sym --wasm --prime bn128 --output ./circuit/
+circom ./circuit/circuit2.circom --r1cs --json --sym --wasm --prime bn128 --output ./circuit/
+circom ./circuit/circuit3.circom --r1cs --json --sym --wasm --prime bn128 --output ./circuit/
+circom ./circuit/cubic_circuit.circom --r1cs --json --sym --wasm --prime bn128 --output ./circuit/
--- src/lib.rs
+++ src/lib.rs
@@ -12,18 +12,21 @@
 
 #[cfg(test)]
 mod tests {
+
     use ark_bn254::{constraints::GVar, Bn254, Fr, G1Projective as G1};
+    use ark_circom::CircomCircuit;
     use ark_grumpkin::{constraints::GVar as GVar2, Projective as G2};
 
     use ark_crypto_primitives::snark::SNARK;
     use ark_groth16::{Groth16, ProvingKey, VerifyingKey as G16VerifierKey};
     use ark_poly_commit::kzg10::VerifierKey as KZGVerifierKey;
 
+    use ark_r1cs_std::{alloc::AllocVar, fields::fp::FpVar};
+    use ark_relations::r1cs::{ConstraintSynthesizer, ConstraintSystem, SynthesisError};
     use ark_std::Zero;
+    use num_bigint::{BigInt, Sign};
 
-    use std::path::PathBuf;
-    use std::time::Instant;
-
+    use ark_ff::PrimeField;
     use folding_schemes::{
         commitment::{
             kzg::{ProverKey as KZGProverKey, KZG},
@@ -35,7 +38,10 @@ mod tests {
             decider_eth_circuit::DeciderEthCircuit,
             get_r1cs, Nova, ProverParams, VerifierParams,
         },
-        frontend::{circom::CircomFCircuit, FCircuit},
+        frontend::{
+            circom::{utils::CircomWrapper, CircomFCircuit},
+            FCircuit,
+        },
         transcript::poseidon::poseidon_test_config,
         Decider, FoldingScheme,
     };
@@ -45,6 +51,12 @@ mod tests {
         verifiers::nova_cyclefold::get_decider_template_for_cyclefold_decider,
         NovaCycleFoldVerifierKey,
     };
+    use std::path::PathBuf;
+    use std::time::Instant;
+
+    use ark_ff::BigInteger;
+    use ark_r1cs_std::R1CSVar;
+    use ark_std::One;
 
     // This method computes the Nova's Prover & Verifier parameters for the example.
     // Warning: this method is only for testing purposes. For a real world use case those parameters
@@ -118,17 +130,145 @@ mod tests {
         (fs_prover_params, kzg_vk, g16_pk, g16_vk)
     }
 
+    pub fn ark_primefield_to_num_bigint(value: Fr) -> BigInt {
+        let primefield_bigint = value.into_bigint();
+        let bytes = primefield_bigint.to_bytes_be();
+        BigInt::from_bytes_be(Sign::Plus, &bytes)
+    }
+
+    fn fpvars_to_bigints(fpVars: Vec<FpVar<Fr>>) -> Result<Vec<BigInt>, SynthesisError> {
+        let mut input_values = Vec::new();
+        // converts each FpVar to PrimeField value, then to num_bigint::BigInt.
+        for fp_var in fpVars.iter() {
+            // extracts the PrimeField value from FpVar.
+            let primefield_value = fp_var.value()?;
+            // converts the PrimeField value to num_bigint::BigInt.
+            let num_bigint_value = ark_primefield_to_num_bigint(primefield_value);
+            input_values.push(num_bigint_value);
+        }
+        Ok(input_values)
+    }
+
     #[test]
-    fn full_flow() {
+    fn cubic_circuit() {
+        let r1cs_path = PathBuf::from("./circuit/cubic_circuit.r1cs");
+        let wasm_path = PathBuf::from("./circuit/cubic_circuit_js/cubic_circuit.wasm");
+
+        let circom_fcircuit = CircomFCircuit::<Fr>::new((r1cs_path, wasm_path, 1, 0)).unwrap(); // state_len:1, external_inputs_len:0
+
+        let cs = ConstraintSystem::<Fr>::new_ref();
+
+        let z_i = vec![Fr::from(3u32)];
+
+        let z_i_var = Vec::<FpVar<Fr>>::new_witness(cs.clone(), || Ok(z_i)).unwrap();
+        let z_i1_var = circom_fcircuit
+            .generate_step_constraints(cs.clone(), 1, z_i_var, vec![])
+            .unwrap();
+        assert_eq!(z_i1_var.value().unwrap(), vec![Fr::from(35u32)]);
+    }
+
+    #[test]
+    fn circuit2() {
         // set the initial state
-        let z_0_aux: Vec<u32> = vec![0_u32; 32 * 8];
-        let z_0: Vec<Fr> = z_0_aux.iter().map(|v| Fr::from(*v)).collect::<Vec<Fr>>();
+        let z_0 = vec![Fr::zero()]
+            .iter()
+            .map(|v| ark_primefield_to_num_bigint(*v))
+            .collect();
+
+        let external_inputs = vec![Fr::from(2), Fr::one(), Fr::one()]
+            .iter()
+            .map(|v| ark_primefield_to_num_bigint(*v))
+            .collect();
+
+        // initialize the Circom circuit
+        let r1cs_path = PathBuf::from("./circuit/circuit2.r1cs");
+        let wasm_path = PathBuf::from("./circuit/circuit2_js/circuit2.wasm");
+        let circom_wrapper = CircomWrapper::new(r1cs_path, wasm_path);
+        let mut inputs_map = vec![
+            ("ivc_input".to_string(), z_0),
+            ("external_input".to_string(), external_inputs),
+        ];
+        let r1cs = circom_wrapper.extract_r1cs().unwrap();
+        let witness = circom_wrapper
+            .extract_witness(&inputs_map)
+            .map_err(|_| SynthesisError::AssignmentMissing)
+            .unwrap();
+
+        let circom_circuit = CircomCircuit {
+            r1cs: r1cs.clone(),
+            witness: Some(witness.clone()),
+            public_inputs_indexes: vec![],
+            allocate_inputs_as_witnesses: true,
+        };
+
+        // Generates the constraints for the circom_circuit.
+        let cs = ConstraintSystem::<Fr>::new_ref();
+        let constraint_generation = circom_circuit.generate_constraints(cs.clone()).unwrap();
+        println!("[CS] Constraint generation is: {:?}", cs.is_satisfied());
+    }
+
+    #[test]
+    fn keccak_simple() {
+        let z_0: Vec<BigInt> = [Fr::zero(); 32 * 8]
+            .iter()
+            .map(|v| ark_primefield_to_num_bigint(*v))
+            .collect();
 
         // initialize the Circom circuit
         let r1cs_path = PathBuf::from("./circuit/keccak-chain.r1cs");
         let wasm_path = PathBuf::from("./circuit/keccak-chain_js/keccak-chain.wasm");
 
-        let f_circuit_params = (r1cs_path, wasm_path, 32 * 8, 0);
+        let circom_wrapper = CircomWrapper::new(r1cs_path, wasm_path);
+        let inputs_map = [("ivc_input".to_string(), z_0)];
+        let r1cs = circom_wrapper.extract_r1cs().unwrap();
+        let witness = circom_wrapper
+            .extract_witness(&inputs_map)
+            .map_err(|_| SynthesisError::AssignmentMissing)
+            .unwrap();
+
+        let circom_circuit = CircomCircuit {
+            r1cs: r1cs.clone(),
+            witness: Some(witness.clone()),
+            public_inputs_indexes: vec![],
+            allocate_inputs_as_witnesses: true,
+        };
+
+        // Generates the constraints for the circom_circuit.
+        let cs = ConstraintSystem::<Fr>::new_ref();
+        _ = circom_circuit.generate_constraints(cs.clone()).unwrap();
+        assert!(cs.is_satisfied().unwrap());
+    }
+
+    #[test]
+    fn circuit3_notfull() {
+        let z_0_aux: Vec<u32> = vec![1, 5, 2];
+        let z_0: Vec<Fr> = z_0_aux.iter().map(|v| Fr::from(*v)).collect::<Vec<Fr>>();
+
+        // initialize the Circom circuit
+        let r1cs_path = PathBuf::from("./circuit/circuit3.r1cs");
+        let wasm_path = PathBuf::from("./circuit/circuit3_js/circuit3.wasm");
+
+        let f_circuit_params = (r1cs_path, wasm_path, 3, 0);
+        let f_circuit = CircomFCircuit::<Fr>::new(f_circuit_params).unwrap();
+
+        let (fs_prover_params, kzg_vk, g16_pk, g16_vk) =
+            init_ivc_and_decider_params::<CircomFCircuit<Fr>>(f_circuit.clone());
+    }
+
+    #[test]
+    fn circuit2_full() {
+        let z_0 = vec![Fr::zero()];
+
+        let external_inputs = vec![
+            vec![Fr::from(0), Fr::one(), Fr::one()],
+            vec![Fr::from(5), Fr::one(), Fr::one()],
+        ];
+
+        // initialize the Circom circuit
+        let r1cs_path = PathBuf::from("./circuit/circuit2.r1cs");
+        let wasm_path = PathBuf::from("./circuit/circuit2_js/circuit2.wasm");
+
+        let f_circuit_params = (r1cs_path, wasm_path, 1, 3);
         let f_circuit = CircomFCircuit::<Fr>::new(f_circuit_params).unwrap();
 
         let (fs_prover_params, kzg_vk, g16_pk, g16_vk) =
@@ -151,12 +291,154 @@ mod tests {
         // initialize the folding scheme engine, in our case we use Nova
         let mut nova = NOVA::init(&fs_prover_params, f_circuit.clone(), z_0).unwrap();
         // run n steps of the folding iteration
-        for i in 0..10 {
+        for i in 0..2 {
             let start = Instant::now();
-            nova.prove_step(vec![]).unwrap();
             println!("Nova::prove_step {}: {:?}", i, start.elapsed());
+            nova.prove_step(external_inputs[i].clone()).unwrap();
         }
 
+        let rng = rand::rngs::OsRng;
+        let start = Instant::now();
+        let proof = DECIDERETH_FCircuit::prove(
+            (g16_pk, fs_prover_params.cs_params.clone()),
+            rng,
+            nova.clone(),
+        )
+        .unwrap();
+        println!("generated Decider proof: {:?}", start.elapsed());
+
+        let verified = DECIDERETH_FCircuit::verify(
+            (g16_vk.clone(), kzg_vk.clone()),
+            nova.i,
+            nova.z_0.clone(),
+            nova.z_i.clone(),
+            &nova.U_i,
+            &nova.u_i,
+            &proof,
+        )
+        .unwrap();
+        assert!(verified);
+        println!("Decider proof verification: {}", verified);
+    }
+
+    #[test]
+    fn circuit3_full() {
+        let z_0_aux: Vec<u32> = vec![0, 0, 0];
+        let z_0: Vec<Fr> = z_0_aux.iter().map(|v| Fr::from(*v)).collect::<Vec<Fr>>();
+
+        // initialize the Circom circuit
+        let r1cs_path = PathBuf::from("./circuit/circuit3.r1cs");
+        let wasm_path = PathBuf::from("./circuit/circuit3_js/circuit3.wasm");
+
+        let f_circuit_params = (r1cs_path, wasm_path, 3, 0);
+        let f_circuit = CircomFCircuit::<Fr>::new(f_circuit_params).unwrap();
+
+        let (fs_prover_params, kzg_vk, g16_pk, g16_vk) =
+            init_ivc_and_decider_params::<CircomFCircuit<Fr>>(f_circuit.clone());
+
+        pub type NOVA =
+            Nova<G1, GVar, G2, GVar2, CircomFCircuit<Fr>, KZG<'static, Bn254>, Pedersen<G2>>;
+        pub type DECIDERETH_FCircuit = DeciderEth<
+            G1,
+            GVar,
+            G2,
+            GVar2,
+            CircomFCircuit<Fr>,
+            KZG<'static, Bn254>,
+            Pedersen<G2>,
+            Groth16<Bn254>,
+            NOVA,
+        >;
+
+        // initialize the folding scheme engine, in our case we use Nova
+        let mut nova = NOVA::init(&fs_prover_params, f_circuit.clone(), z_0).unwrap();
+        // run n steps of the folding iteration
+        for i in 0..5 {
+            let start = Instant::now();
+            println!("Nova::prove_step {}: {:?}", i, start.elapsed());
+            nova.prove_step(vec![]).unwrap();
+        }
+
+        let rng = rand::rngs::OsRng;
+        let start = Instant::now();
+        let proof = DECIDERETH_FCircuit::prove(
+            (g16_pk, fs_prover_params.cs_params.clone()),
+            rng,
+            nova.clone(),
+        )
+        .unwrap();
+        println!("generated Decider proof: {:?}", start.elapsed());
+
+        let verified = DECIDERETH_FCircuit::verify(
+            (g16_vk.clone(), kzg_vk.clone()),
+            nova.i,
+            nova.z_0.clone(),
+            nova.z_i.clone(),
+            &nova.U_i,
+            &nova.u_i,
+            &proof,
+        )
+        .unwrap();
+        assert!(verified);
+        println!("Decider proof verification: {}", verified);
+    }
+
+    #[test]
+    fn full_flow() {
+        // set the initial state
+        let z_0_aux: Vec<u32> = vec![0_u32; 8];
+        let z_0: Vec<Fr> = z_0_aux.iter().map(|v| Fr::from(*v)).collect::<Vec<Fr>>();
+
+        // initialize the Circom circuit
+        let r1cs_path = PathBuf::from("./circuit/keccak-chain.r1cs");
+        let wasm_path = PathBuf::from("./circuit/keccak-chain_js/keccak-chain.wasm");
+
+        let f_circuit_params = (r1cs_path, wasm_path, 8, 0);
+        let f_circuit = CircomFCircuit::<Fr>::new(f_circuit_params).unwrap();
+
+        let (fs_prover_params, kzg_vk, g16_pk, g16_vk) =
+            init_ivc_and_decider_params::<CircomFCircuit<Fr>>(f_circuit.clone());
+
+        pub type NOVA =
+            Nova<G1, GVar, G2, GVar2, CircomFCircuit<Fr>, KZG<'static, Bn254>, Pedersen<G2>>;
+        pub type DECIDERETH_FCircuit = DeciderEth<
+            G1,
+            GVar,
+            G2,
+            GVar2,
+            CircomFCircuit<Fr>,
+            KZG<'static, Bn254>,
+            Pedersen<G2>,
+            Groth16<Bn254>,
+            NOVA,
+        >;
+
+        // initialize the folding scheme engine, in our case we use Nova
+        let mut nova = NOVA::init(&fs_prover_params, f_circuit.clone(), z_0.clone()).unwrap();
+        // run n steps of the folding iteration
+        for i in 0..4 {
+            println!("Nova::prove_step {}", i);
+            nova.prove_step(vec![]).unwrap();
+        }
+
+        let verifier_params = VerifierParams::<G1, G2> {
+            poseidon_config: nova.poseidon_config.clone(),
+            r1cs: nova.clone().r1cs,
+            cf_r1cs: nova.clone().cf_r1cs,
+        };
+        let (running_instance, incoming_instance, cyclefold_instance) = nova.instances();
+        let verified = NOVA::verify(
+            verifier_params,
+            z_0,
+            nova.z_i.clone(),
+            nova.i,
+            running_instance,
+            incoming_instance,
+            cyclefold_instance,
+        )
+        .unwrap();
+
+        println!("VERIFIED: {:?}", verified);
         let rng = rand::rngs::OsRng;
         let start = Instant::now();
         let proof = DECIDERETH_FCircuit::prove(
